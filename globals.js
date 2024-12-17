14f7cca1a40170b59884ad24bd61722013a0cd11
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2024-12-17 09:09:39 +0100

    añadir las imagenes y el nombre de las monedas

--- globals.js
+++ globals.js
@@ -1,25 +1,25 @@
 // globals.js
 
 // Inicializamos con el valor de localStorage si existe
-let creditos = obtenerCreditos();  
+let KnowCoins = obtenerKnowCoins();  
 
-// Función para obtener los créditos desde localStorage
-function obtenerCreditos() {
-    const creditosGuardados = localStorage.getItem('creditos');
-    return creditosGuardados ? parseInt(creditosGuardados) : 0;  // Si no existe, devolvemos 0
+// Función para obtener los KnowCoins desde localStorage
+function obtenerKnowCoins() {
+    const KnowCoinsGuardados = localStorage.getItem('KnowCoins');
+    return KnowCoinsGuardados ? parseInt(KnowCoinsGuardados) : 0;  // Si no existe, devolvemos 0
 }
 
-// Función para actualizar los créditos en localStorage
-function actualizarCreditos(nuevaCantidad) {
-    creditos = nuevaCantidad;
-    localStorage.setItem('creditos', nuevaCantidad);  // Guardamos en localStorage
+// Función para actualizar los KnowCoins en localStorage
+function actualizarKnowCoins(nuevaCantidad) {
+    KnowCoins = nuevaCantidad;
+    localStorage.setItem('KnowCoins', nuevaCantidad);  // Guardamos en localStorage
 }
 
 // Esta función actualizará el valor en la página (si es necesario)
-function actualizarCreditosEnPagina() {
-    // Seleccionamos el elemento de la página donde se mostrarán los créditos
-    const creditosDisplay = document.getElementById("creditos-display");
-    if (creditosDisplay) {
-        creditosDisplay.textContent = `${creditos} `;
+function actualizarKnowCoinsEnPagina() {
+    // Seleccionamos el elemento de la página donde se mostrarán los KnowCoins
+    const KnowCoinsDisplay = document.getElementById("KnowCoins-display");
+    if (KnowCoinsDisplay) {
+        KnowCoinsDisplay.textContent = `${KnowCoins} `;
     }
 }
